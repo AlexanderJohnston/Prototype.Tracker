628f22e32edcf4d83b9307c1ff6fe9b7c50c7f43
Alexander Johnston <ajohnston@ulabs.io>
2017-11-04 02:57:25 -0400

    Initial commit

---
+++ UserInterface/Model/ActiveWindow.cs
@@ -0,0 +1,64 @@
+﻿#region header
+
+// UserInterface - ActiveWindow.cs
+// 
+// Copyright Untethered Labs, Inc.  All rights reserved.
+// 
+// Created: 2017-11-04 1:35 AM
+
+#endregion
+
+#region using
+
+using System ;
+using System.Diagnostics ;
+using System.Runtime.InteropServices ;
+using System.Text ;
+using System.Threading ;
+using System.Threading.Tasks ;
+
+using PostSharp.Patterns.Model ;
+
+#endregion
+
+namespace UserInterface.Model
+{
+    [NotifyPropertyChanged]
+    public class ActiveWindow
+    {
+        private Timer stateTimer ;
+
+        internal ActiveWindow ()
+        {
+            //  0ms initial fire and each 100ms thereafter.
+            stateTimer = new Timer (this.UpdateTimer, null, 0, 100) ;
+        }
+
+        private void UpdateTimer (Object stateInfo)
+        {
+            this.Text = this.GetActiveWindowTitle();
+        }
+
+        public string Text { get ; set ; }
+
+        #region Windows API Call
+
+        [DllImport ("user32.dll")]
+        private static extern IntPtr GetForegroundWindow () ;
+
+
+        [DllImport ("user32.dll")]
+        private static extern int GetWindowText (IntPtr hWnd, StringBuilder text, int count) ;
+
+        private string GetActiveWindowTitle ()
+        {
+            const int nChars = 256 ;
+            var Buff = new StringBuilder (nChars) ;
+            IntPtr handle = ActiveWindow.GetForegroundWindow () ;
+
+            return ActiveWindow.GetWindowText (handle, Buff, nChars) > 0 ? Buff.ToString () : null ;
+        }
+
+        #endregion
+    }
+}
---
+++ UserInterface/ViewModel/InterfaceViewModel.cs
@@ -0,0 +1,56 @@
+﻿#region header
+
+// UserInterface - InterfaceViewModel.cs
+// 
+// Copyright Untethered Labs, Inc.  All rights reserved.
+// 
+// Created: 2017-11-04 1:22 AM
+
+#endregion
+
+#region using
+
+using System.ComponentModel ;
+using System.Threading.Tasks ;
+
+using PostSharp ;
+using PostSharp.Patterns.Model ;
+
+using UserInterface.Model ;
+
+#endregion
+
+namespace UserInterface.ViewModel
+{
+    [NotifyPropertyChanged]
+    public class InterfaceViewModel
+    {
+        public InterfaceViewModel()
+        {
+            this.Test = "Hi";
+
+            this.actWinModel = new ActiveWindow();
+
+            Post.Cast<ActiveWindow, INotifyPropertyChanged>
+                (this.actWinModel).PropertyChanged += this.RefreshActiveWindow;
+
+            this.Looper();
+        }
+
+        private readonly ActiveWindow actWinModel ;
+
+        public string Test { get ; set ; }
+
+        private void RefreshActiveWindow (object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName == "Text")
+                this.Test = this.actWinModel.Text ;
+        }
+
+        private async void Looper ()
+        {
+            await Task.Delay(3000);
+            this.Looper();
+        }
+    }
+}
